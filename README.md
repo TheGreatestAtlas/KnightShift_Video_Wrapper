76e8d41f1f2313b5d3b2c0cb7d6467fa55f19385
TheGreatestAtlas <162817701+TheGreatestAtlas@users.noreply.github.com>
2025-09-25 13:46:13 +0200

    init

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
-# KnightShift_Video_Wrapper
+# KnightShift Video Wrapper
+
+# PL:
+
+## Opis
+Ten program umożliwia zastąpienie oryginalnych plików wideo intro w grze **KnightShift** dowolnym formatem wideo.  
+Zamiast plików `.twv` uruchamiane są materiały wideo przez **ffplay.exe** w trybie pełnoekranowym.  
+Rozwiązanie działa wyłącznie dla filmów intro odtwarzanych przy starcie gry.
+
+## Jak to działa
+1. Program wywołuje obrazek `SPG.wd1`.  
+2. Odtwarza wszystkie pliki wideo znajdujące się w katalogu `Video` przy użyciu `ffplay.exe`.  
+3. Po zakończeniu uruchamia główny plik `.exe` gry.  
+
+## Wymagania
+- `ffplay.exe` (część pakietu [FFmpeg](https://ffmpeg.org/))  
+- Zainstalowana gra **KnightShift**  
+
+## Instalacja i użycie
+1. Skopiuj pliki wrappera do katalogu gry.  
+2. Upewnij się, że w folderze `Video` znajdują się pliki wideo, które chcesz odtworzyć.  
+3. Uruchom wrapper zamiast oryginalnego `.exe` gry.  
+
+---
+
+# EN:
+
+## Description
+This program allows you to replace the original intro video files in the game **KnightShift** with any video format.  
+Instead of `.twv` files, videos are played by **ffplay.exe** in full-screen mode.  
+This solution only works for intro videos played at the start of the game.
+
+## How it works
+1. The program calls the image `SPG.wd1`.  
+2. It plays all video files located in the `Video` directory using `ffplay.exe`.  
+3. When finished, it launches the main `.exe` file of the game.  
+
+## Requirements
+- `ffplay.exe` (part of the [FFmpeg](https://ffmpeg.org/) package)  
+- **KnightShift** game installed  
+
+## Installation and use
+1. Copy the wrapper files to the game directory.  
+2. Make sure that the `Video` folder contains the video files you want to play.  
+3. Run the wrapper instead of the original game `.exe`.  
+
---
+++ RunGoldEdition/RunGoldEdition/RunGoldEdition.cpp
@@ -0,0 +1,187 @@
+﻿#include <windows.h>
+#include <string>
+#include <filesystem>
+#include <fstream>
+
+#include "resource.h"
+
+using namespace std;
+
+namespace fs = filesystem;
+
+// .Global
+HBITMAP hBitmap = NULL; // loading picture
+LPCWSTR szClassName = L"WindowClass"; // window class name
+
+const char wd_files_dir_name[] = "WDFiles";
+const char video_dir_name[] = "Video";
+const char spg_file_name[] = "SPG.wd1";
+const char run_gold_edition_cfg_file_name[] = "RunGoldEdition.cfg";
+
+// Window Function
+static LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+    switch (uMsg)
+    {
+        case WM_PAINT:
+        {
+            PAINTSTRUCT ps;
+            HDC hdc = BeginPaint(hwnd, &ps);
+
+            // Create a DC for drawing an image
+            HDC hdcMem = CreateCompatibleDC(hdc);
+            SelectObject(hdcMem, ::hBitmap);
+
+            // Get bitmap information
+            BITMAP bitmap = {};
+            GetObject(::hBitmap, sizeof(BITMAP), &bitmap);
+
+            // Draw a picture on the window
+            BitBlt(hdc, 0, 0, bitmap.bmWidth, bitmap.bmHeight, hdcMem, 0, 0, SRCCOPY);
+
+            DeleteDC(hdcMem);
+            EndPaint(hwnd, &ps);
+        }
+        break;
+
+        case WM_DESTROY:
+            PostQuitMessage(0);
+        break;
+
+        default:
+            return DefWindowProc(hwnd, uMsg, wParam, lParam);
+    }
+    return 0;
+}
+
+// main func
+static int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow)
+{
+    // Load image (bitmap from file)
+
+    wstring spg_file_path = (fs::current_path() / ::wd_files_dir_name / ::spg_file_name).wstring();
+
+    hBitmap = (HBITMAP)LoadImage(NULL, spg_file_path.c_str(), IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE);
+    if (hBitmap == NULL)
+    {
+        MessageBox(NULL, L"The image could not be loaded!", L"Error", MB_ICONERROR);
+        return -1;
+    }
+
+    // Get bitmap information
+    BITMAP bitmap = {};
+    GetObject(hBitmap, sizeof(BITMAP), &bitmap);
+
+    // Get screen dimensions
+    int screenWidth = GetSystemMetrics(SM_CXSCREEN);
+    int screenHeight = GetSystemMetrics(SM_CYSCREEN);
+
+    // Calculate the position of the window in the center of the screen
+    int xPos = (screenWidth - bitmap.bmWidth) / 2;
+    int yPos = (screenHeight - bitmap.bmHeight) / 2;
+
+    // Register the window class
+    WNDCLASS wc = { 0 };
+    wc.lpfnWndProc = WindowProc;
+    wc.hInstance = hInstance;
+    wc.lpszClassName = szClassName;
+    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wc.hbrBackground = (HBRUSH)GetStockObject(NULL_BRUSH); // Brak tła
+    RegisterClass(&wc);
+
+    
+
+    // Create a window without a frame or title bar (WS_POPUP style) at the specified position
+    HWND hwnd = CreateWindowEx
+    (
+        WS_EX_LAYERED,                   // Extended style: transparent window
+        szClassName,                     // Window class name
+        NULL,                            // No window title
+        WS_POPUP,                        // Frameless window style
+        xPos, yPos,                      // Window position (center of screen)
+        bitmap.bmWidth, bitmap.bmHeight, // Window size adjusted to the image
+        NULL,                            // No parent
+        NULL,                            // No menu
+        hInstance,                       // Instance handle
+        NULL                             // Additional parameters
+    );
+
+    if (hwnd == NULL)
+    {
+        return 0;
+    }
+
+    auto hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_ICON1));
+    SendMessage(hwnd, WM_SETICON, ICON_SMALL, (LPARAM)hIcon);
+    SendMessage(hwnd, WM_SETICON, ICON_SMALL2, (LPARAM)hIcon);
+    SendMessage(hwnd, WM_SETICON, ICON_BIG, (LPARAM)hIcon);
+
+
+    // Set the window transparency and green color as transparent
+    COLORREF greenColor = RGB(3, 222, 3); // Green color (RGB)
+    SetLayeredWindowAttributes(hwnd, greenColor, 255, LWA_COLORKEY);
+
+    // Show window
+    ShowWindow(hwnd, SW_SHOW);
+
+    // Display the window for 1 second
+    MSG msg = { 0 };
+    DWORD startTime = GetTickCount64();
+    while (GetTickCount64() - startTime < 500) // The loop runs for 1 second
+    {
+        if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+        {
+            if (msg.message == WM_QUIT)
+                break;
+
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+    }
+
+    // Destroy the window after 3 seconds
+    DestroyWindow(hwnd);
+
+    // Release resources
+    DeleteObject(hBitmap);
+
+    fs::path video_dir_path = fs::current_path() / ::video_dir_name;
+
+    fs::create_directory(video_dir_path);
+
+    for (auto& itm : fs::directory_iterator(video_dir_path))
+    {
+        if (!itm.is_directory())
+        { 
+            //debugf << "ffplay.exe -i " << itm.path().string() << " -fs -autoexit" << std::endl;
+            system(("ffplay.exe -i " + itm.path().string() + " -fs -autoexit").c_str());
+        }
+
+    }
+
+    fs::path config_file_path = fs::current_path() / ::run_gold_edition_cfg_file_name;
+
+    if (!fs::exists(config_file_path))
+    {
+        ofstream out(config_file_path);
+        out << "LAUNCHER=\"KnightShift.exe\"\n";
+    }
+
+    ifstream in(config_file_path);
+    string line;
+    getline(in, line);
+
+    string launcher;
+    auto pos = line.find('=');
+
+    if (pos != string::npos) 
+    {
+        launcher = line.substr(pos + 1);
+        if (!launcher.empty() && launcher.front() == '"') launcher.erase(0, 1);
+        if (!launcher.empty() && launcher.back() == '"') launcher.pop_back();
+    }
+
+    system(launcher.c_str());
+  
+    return 0;
+}
